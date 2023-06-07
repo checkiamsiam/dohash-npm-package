639b4d56cc233bd3f81a321f5404045a2b699318
checkiamsiam <issiam02415@gmail.com>
2023-06-07 17:03:20 +0600

    remove extra

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,2 @@
-export const secretExtra: string = "sEcreTExtrACHeck";
-export const strExtra: string = "paSSwOrdExtRAChECk";
 export const secretExist: string = "seCrEtexiST";
 export const checkValid: string = "chEckSiaMhaShInG";
